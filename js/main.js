8d0dbe5c7feed6f1c6386bb5e4288e3bbb05b38c
caraagard <caraagard@byui.edu>
2021-10-22 14:47:38 -0700

    week2

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+const notes = ["week1.html","week2.html","week3.html","week4.html",
+"week5.html","week6.html","week7.html","week8.html","week9.html",
+"week10.html","week11.html","week12.html","week13.html","week14.html"]; 
+var result = ""
+for (var i = 0; i < notes.length; i++) {
+    result = result + " <a href='" + notes[i] + "'>"+ notes[i] + "</a>";
+}
+document.getElementById('week').innerHTML = result
+function myFunction(value, index, array) {
+    txt += value + "<br>"; 
+  }
